3c3e0254b33e9ff029b9f61438dfd37601b157a6
addxeles <knightofthenight1@gmail.com>
2019-12-27 17:04:08 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Design_Pattern
+{
+    #region Single Responsibility in SOLID principal
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+    // separate out saving responsibility for a separate class
+    public class Persistance
+    {
+        public void SaveToFile(string fileName, Journal journal, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, journal.ToString());
+            }
+        }
+    }
+
+    #endregion
+
+    #region Open_Close in SOLID
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+                throw new ArgumentNullException(paramName: nameof(name));
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                    yield return p;
+            }
+        }
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                    yield return p;
+            }
+        }
+
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsStatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specification);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsStatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        Size size;
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsStatisfied(Product product)
+        {
+            return product.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));;
+            this.second = second ?? throw new ArgumentOutOfRangeException(paramName: nameof(second));;           
+        }
+        public bool IsStatisfied(T t)
+        {
+            return first.IsStatisfied(t) && second.IsStatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+            {
+                if (spec.IsStatisfied(i))
+                    yield return i;
+            }
+        }
+    }
+
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("Hello");
+            j.AddEntry("world");
+            Console.WriteLine(j);
+        }
+    }
+}
